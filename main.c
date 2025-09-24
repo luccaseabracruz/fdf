6e747c5e47af706c24ebaa0d37e39a0c7f8e8957
Lucca Cruz <lucca1609.ls@gmail.com>
2025-09-24 20:03:19 +0100

    fix: validate before window creation.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:51:01 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/09/24 18:51:42 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/09/24 20:02:41 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	main(int argc, char **argv)
 	if (!validate_input(argc, argv))
 		return (EXIT_FAILURE);
 	ft_bzero(&dt, sizeof(t_data));
-	init_win(&dt);
 	init_map(&dt, argv);
+	init_win(&dt);
 	init_view(&dt);
 	render(&dt);
 	hooks(&dt);
