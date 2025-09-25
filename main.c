a7ae708d0ec6c3748cfe081bfbbe40740490deed
Lucca Cruz <lucca1609.ls@gmail.com>
2025-09-25 10:54:22 +0100

    feat: main documentation.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:51:01 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/09/24 20:02:41 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/09/25 10:49:49 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,24 @@
 #include "libft.h"
 #include "mlx.h"
 #include <stdlib.h>
-#include <unistd.h>
 
+/**
+ * @brief Entry point of the mandatory FdF program.
+ * Note: bonus features are compiled separately in @ref main_bonus.c.
+ *
+ * This function initializes the application by:
+ * - Validating the program input.
+ * - Initializing the map data.
+ * - Creating the window and setting up the view.
+ * - Rendering the initial frame.
+ * - Registering hooks for user interaction.
+ * - Starting the MLX event loop.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line argument strings.
+ * @return EXIT_SUCCESS (0) on normal execution,
+ *         EXIT_FAILURE (1) if the input is invalid.
+ */
 int	main(int argc, char **argv)
 {
 	t_data	dt;
--- main_bonus.c
+++ main_bonus.c
@@ -6,15 +6,26 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:51:01 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/09/24 20:00:41 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/09/25 10:50:40 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file main_bonus.c
+ * @brief Entry point of the bonus FdF program.
+ *
+ * This file initializes and runs the extended version of the FdF program,
+ * including additional features such as enhanced controls and rendering options.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line argument strings.
+ * @return EXIT_SUCCESS (0) on normal execution,
+ *         EXIT_FAILURE (1) if the input is invalid.
+ */
 #include "fdf.h"
 #include "libft.h"
 #include "mlx.h"
 #include <stdlib.h>
-#include <unistd.h>
 
 int	main(int argc, char **argv)
 {
