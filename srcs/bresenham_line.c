a1e1fc2a3dbef632b96fe1e124f6991080a71f99
Lucca Cruz <lucca1609.ls@gmail.com>
2025-09-25 12:15:41 +0100

    doc: bresenham_line.c documentation.

--- srcs/bresenham_line.c
+++ srcs/bresenham_line.c
@@ -6,13 +6,24 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 15:43:17 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/09/24 16:16:48 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/09/25 12:15:03 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "libft.h"
 
+/**
+ * @brief Plot a pixel at the given coordinates.
+ *
+ * This helper function writes a single pixel to the image buffer
+ * if the coordinates are inside the window bounds.
+ *
+ * @param dt    Main program data containing image buffer info.
+ * @param x     X coordinate of the pixel.
+ * @param y     Y coordinate of the pixel.
+ * @param color Color value to write (ARGB).
+ */
 static void	pixel_put(t_data *dt, int x, int y, int color)
 {
 	char	*dst;
@@ -24,6 +35,16 @@ static void	pixel_put(t_data *dt, int x, int y, int color)
 	}
 }
 
+/**
+ * @brief Initialize a Bresenham line drawing context.
+ *
+ * Prepares the delta values, step directions, and endpoints
+ * needed for the Bresenham algorithm.
+ *
+ * @param b_dt Pointer to a Bresenham context struct.
+ * @param p1   Starting point of the line.
+ * @param p2   Ending point of the line.
+ */
 static void	init_bresenham(t_bresenham *b_dt, t_point p1, t_point p2)
 {
 	ft_bzero(b_dt, sizeof(b_dt));
@@ -41,6 +62,21 @@ static void	init_bresenham(t_bresenham *b_dt, t_point p1, t_point p2)
 	b_dt->p2 = p2;
 }
 
+/**
+ * @brief Draw a shallow line using Bresenham's algorithm.
+ *
+ * Used when the line slope is less than 1.
+ *
+ * @param dt   Main program data containing image buffer info.
+ * @param b_dt Bresenham line context initialized with @ref init_bresenham.
+ * 
+ * Local variables:
+ * - `x`: Current X coordinate during drawing.
+ * - `y`: Current Y coordinate during drawing.
+ * - `p`: Decision parameter that determines when to step in X.
+ * - `i`: Step counter used to traverse the line.
+ * - `cur_color`: Interpolated color at the current step.
+ */
 static void	shallow_line(t_data *dt, t_bresenham b_dt)
 {
 	int	x;
@@ -69,6 +105,21 @@ static void	shallow_line(t_data *dt, t_bresenham b_dt)
 	}
 }
 
+/**
+ * @brief Draw a steep line using Bresenham's algorithm.
+ *
+ * Used when the line slope is greater than or equal to 1.
+ *
+ * @param dt   Main program data containing image buffer info.
+ * @param b_dt Bresenham line context initialized with @ref init_bresenham.
+ * 
+ * Local variables:
+ * - `x`: Current X coordinate during drawing.
+ * - `y`: Current Y coordinate during drawing.
+ * - `p`: Decision parameter that determines when to step in X.
+ * - `i`: Step counter used to traverse the line.
+ * - `cur_color`: Interpolated color at the current step.
+ */
 static void	steep_line(t_data *dt, t_bresenham b_dt)
 {
 	int	x;
@@ -97,6 +148,19 @@ static void	steep_line(t_data *dt, t_bresenham b_dt)
 	}
 }
 
+/**
+ * @brief Draw a line between two points using Bresenham's algorithm.
+ *
+ * Chooses between @ref shallow_line and @ref steep_line
+ * depending on the line slope.
+ *
+ * @param dt Main program data containing image buffer info.
+ * @param p1 Starting point of the line.
+ * @param p2 Ending point of the line.
+ *
+ * Local variables:
+ * - `b_dt`: The Bresenham context holding deltas, steps, and endpoints.
+ */
 void	bresenham_line(t_data *dt, t_point p1, t_point p2)
 {
 	t_bresenham	b_dt;
